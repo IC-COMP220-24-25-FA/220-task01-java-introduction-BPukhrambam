fc8b48cce4006c0c9e4b8480c3f51821def00db9
baneetpukhrambam <bpukhrambam@ithaca.edu>
2024-09-01 18:46:48 -0400

    Added code for Triangle calcArea

--- src/main/java/edu/ithaca/dragon/shapes/Triangle.java
+++ src/main/java/edu/ithaca/dragon/shapes/Triangle.java
@@ -12,7 +12,9 @@ public class Triangle {
     }
 
     public double calcArea(){
-        throw new RuntimeException("Not Implemented");
+        double semiPerimeter = (side1 + side2 + side3)/2;
+        double area = Math.sqrt(semiPerimeter * (semiPerimeter - side1) * (semiPerimeter - side2) * (semiPerimeter - side3));
+        return area;
     }
 
     public void doubleSize(){
