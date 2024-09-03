23eb187c2fec409fcfb838cb595962997866da72
baneetpukhrambam <bpukhrambam@ithaca.edu>
2024-09-03 10:03:43 -0400

    Added comments on largestofThreeTest

--- src/main/java/edu/ithaca/dragon/practice/FunctionPractice.java
+++ src/main/java/edu/ithaca/dragon/practice/FunctionPractice.java
@@ -8,47 +8,52 @@ public class FunctionPractice {
      * @return the largest of the 3 numbers
      * @throws IllegalArgumentException if any of the numbers is negative
      */
-    public static int largestOfThree(int first, int second, int third){
-        throw new RuntimeException("Not Implemented");
-    }
-    
-    /**
-     * @return the final price at register of the given item after discount and tax applied
-     */
-    public static double calcSalePrice(double originalPrice, double discountPercent, double salesTax){
+    public static int largestOfThree(int first, int second, int third) {
         throw new RuntimeException("Not Implemented");
     }
 
     /**
-     * @return true if the data collected shows the dog is good, false if bad dog today
+     * @return the final price at register of the given item after discount and tax
+     *         applied
      */
-    public static boolean isGoodDog(int yearsOld, int daysSinceShoesChewed, boolean fetchedThePaperToday){
+    public static double calcSalePrice(double originalPrice, double discountPercent, double salesTax) {
         throw new RuntimeException("Not Implemented");
     }
 
     /**
-     * @return the index of the largest number in the list, or -1 if the list is empty.
-     * If the largest number occurs more than once, return the index of the first occurence.
+     * @return true if the data collected shows the dog is good, false if bad dog
+     *         today
      */
-    public static int findFirstLargest(List<Integer> numbers){
+    public static boolean isGoodDog(int yearsOld, int daysSinceShoesChewed, boolean fetchedThePaperToday) {
         throw new RuntimeException("Not Implemented");
     }
 
     /**
-     * @return the index of the largest number in the list, or -1 if the list is empty.
-     * If the largest number occurs more than once, return the index of the last occurence.
+     * @return the index of the largest number in the list, or -1 if the list is
+     *         empty.
+     *         If the largest number occurs more than once, return the index of the
+     *         first occurence.
      */
-    public static int findLastLargest(List<Integer> numbers){
+    public static int findFirstLargest(List<Integer> numbers) {
+        throw new RuntimeException("Not Implemented");
+    }
+
+    /**
+     * @return the index of the largest number in the list, or -1 if the list is
+     *         empty.
+     *         If the largest number occurs more than once, return the index of the
+     *         last occurence.
+     */
+    public static int findLastLargest(List<Integer> numbers) {
         throw new RuntimeException("Not Implemented");
     }
 
     /**
      * @return the string that has contains the most occurences of the given letter
-     * @throws 
+     * @throws
      */
-    public static String findFirstMostOccurencesOfLetter(List<String> words, char letter){
+    public static String findFirstMostOccurencesOfLetter(List<String> words, char letter) {
         throw new RuntimeException("Not Implemented");
     }
-
 
 }
--- src/test/java/edu/ithaca/dragon/practice/FunctionPracticeTest.java
+++ src/test/java/edu/ithaca/dragon/practice/FunctionPracticeTest.java
@@ -11,20 +11,53 @@ import org.junit.jupiter.api.Test;
 public class FunctionPracticeTest {
 
     @Test
-    public void largestOfThreeTest(){
-        assertEquals(5, FunctionPractice.largestOfThree(4, 5, 2));
-        assertEquals(5, FunctionPractice.largestOfThree(5, 4, 2));
-        assertEquals(5, FunctionPractice.largestOfThree(4, 2, 5));
-        assertEquals(5, FunctionPractice.largestOfThree(5, 5, 5));
+    public void largestOfThreeTest() {
+        assertEquals(5, FunctionPractice.largestOfThree(4, 5, 2)); /*
+                                                                    * verifies if largest number
+                                                                    * is retrieved when it is the second number
+                                                                    */
+        assertEquals(5, FunctionPractice.largestOfThree(5, 4, 2)); /*
+                                                                    * verifies if largest number
+                                                                    * is retrieved when it is the first number
+                                                                    */
+        assertEquals(5, FunctionPractice.largestOfThree(4, 2, 5)); /*
+                                                                    * verifies if largest number
+                                                                    * is retrieved when it is the third number
+                                                                    */
+        assertEquals(5, FunctionPractice.largestOfThree(5, 5, 5)); /*
+                                                                    * verifies if largest number
+                                                                    * is retrieved when all numbers are equal
+                                                                    */
 
-        assertThrows(IllegalArgumentException.class, () -> FunctionPractice.largestOfThree(-1, -1, -1));
-        assertThrows(IllegalArgumentException.class, () -> FunctionPractice.largestOfThree(5, -1, 3));
-        assertThrows(IllegalArgumentException.class, () -> FunctionPractice.largestOfThree(5, 3, -1));
+        assertThrows(IllegalArgumentException.class, () -> FunctionPractice.largestOfThree(-1, -1, -1)); /*
+                                                                                                          * verifies
+                                                                                                          * if an error
+                                                                                                          * is thrown
+                                                                                                          * when all
+                                                                                                          * numbers are
+                                                                                                          * negative
+                                                                                                          */
+        assertThrows(IllegalArgumentException.class, () -> FunctionPractice.largestOfThree(5, -1, 3)); /*
+                                                                                                        * verifies
+                                                                                                        * if an error is
+                                                                                                        * thrown when
+                                                                                                        * the second
+                                                                                                        * number is
+                                                                                                        * negative
+                                                                                                        */
+        assertThrows(IllegalArgumentException.class, () -> FunctionPractice.largestOfThree(5, 3, -1)); /*
+                                                                                                        * verifies
+                                                                                                        * if an error is
+                                                                                                        * thrown when
+                                                                                                        * the third
+                                                                                                        * number is
+                                                                                                        * negative
+                                                                                                        */
     }
 
     @Test
-    public void calcSalePriceTest(){
+    public void calcSalePriceTest() {
         fail("Not implemented yet");
     }
-    
+
 }
