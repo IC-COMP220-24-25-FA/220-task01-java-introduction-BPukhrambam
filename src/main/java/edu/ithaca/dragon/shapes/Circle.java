eb068443922bf0db1c2e97e692012cb61c5d9fbd
baneetpukhrambam <bpukhrambam@ithaca.edu>
2024-09-04 10:40:18 -0400

    added Shape.java file

--- src/main/java/edu/ithaca/dragon/shapes/Circle.java
+++ src/main/java/edu/ithaca/dragon/shapes/Circle.java
@@ -1,38 +1,38 @@
 package edu.ithaca.dragon.shapes;
 
-public class Circle {
+public class Circle implements Shape {
     private double radius;
 
     /**
      * @throws IllegalArgumentException if radius is not a positive number
      */
-    public Circle(double radius){
+    public Circle(double radius) {
         this.radius = radius;
     }
 
     /**
      * @return the area of this circle
      */
-    public double calcArea(){
+    public double calcArea() {
         double pi = 3.14159;
-        return  pi * this.radius * this.radius;
+        return pi * this.radius * this.radius;
     }
 
     /**
      * @post doubles the size of this circle
      */
-    public void doubleSize(){
+    public void doubleSize() {
         this.radius = this.radius * 2;
     }
 
     /**
      * @return the longest straight line that can be drawn within this circle
      */
-    public double longestLineWithin(){
+    public double longestLineWithin() {
         return 2 * this.radius;
     }
 
-    public double getRadius(){
-        return  this.radius;
+    public double getRadius() {
+        return this.radius;
     }
 }
